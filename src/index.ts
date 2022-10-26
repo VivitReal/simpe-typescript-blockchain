75940f710fa6fc8b27de33d662bf13c60d1d5e9a
VivitReal <vivitman95x@gmail.com>
2022-10-26 17:48:25 +0300

    feat: create class Block for blockchain feat: add typescript watcher for dynamic update code

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,27 @@
-console.log('hello')
+class Block {
+  public id: number
+  public hash: string
+  public prevHash: string
+  public data: string
+  public timestamp: number
+  constructor(
+    id: number,
+    hash: string,
+    prevHash: string,
+    data: string,
+    timestamp: number
+  )
+  {
+    this.id = id
+    this.hash = hash
+    this.prevHash = prevHash
+    this.data = data
+    this.timestamp = timestamp
+  }
+}
+
+let blockchain: [Block]
+
+export {
+  blockchain
+}
